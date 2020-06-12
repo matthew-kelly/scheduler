72ce034b529e1e3f1e18efaa055fbf36e7b4b31d
Matt Kelly <bmatthewkelly@gmail.com>
2020-06-12 16:08:46 -0700

    add Axios package, fetch Days from server

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const sampleAppointments = [
   {
@@ -71,10 +54,18 @@ const sampleAppointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
 
   const appointments = sampleAppointments.map((appt) => {
     return <Appointment key={appt.id} {...appt} />;
   });
+
+  // Fetch days, only run once
+  useEffect(() => {
+    axios.get("/api/days").then((res) => {
+      setDays(res.data);
+    });
+  }, []);
 
   return (
     <main className="layout">
